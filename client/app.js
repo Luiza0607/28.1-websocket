c75fb01361deffc641eff718fe2718f14911620f
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-03-04 15:23:32 +0100

    add websocket

--- client/app.js
+++ client/app.js
@@ -5,6 +5,7 @@ const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
 
+const socket = io();
 
 let userName;
 
@@ -14,26 +15,32 @@ const login = (e) => {
 		alert('Must not be empty !');
 	} else {
 		userName = userNameInput.value;
+		socket.emit('userName', userName);
 		loginForm.classList.remove('show');
 		messagesSection.classList.add('show');
 	}
 };
-
 const sendMessage = (e) => {
 	e.preventDefault();
 	if (!messageContentInput.value) {
 		alert('You must write the message');
 	} else {
 		addMessage(userName, messageContentInput.value);
+		socket.emit('message', {
+			author: userName,
+			content: messageContentInput.value,
+		});
 		messageContentInput.value = '';
 	}
 };
-
 const addMessage = (author, content) => {
 	const message = document.createElement('li');
 	message.classList.add('message', 'message--received');
 	if (author === userName) {
 		message.classList.add('message--self');
+	}
+	if (author === 'Chat Bot') {
+		message.classList.add('message--bot');
 	}
 	message.innerHTML = ` <h3 class="message__author">${author === userName ? 'You' : author
 		}</h3>
@@ -41,6 +48,8 @@ const addMessage = (author, content) => {
   </li>`;
 	messagesList.appendChild(message);
 };
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
 
 loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage);
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
+const users = [];
 
 app.use(express.static(path.join(__dirname, '/client')));
 
@@ -11,6 +13,40 @@ app.get('/', (_req, res) => {
 	res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
 	console.log('server running on port 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+	console.log('New client! Its id – ' + socket.id);
+
+	socket.on('userName', (user) => {
+		console.log(socket.id + " has chosen the name! it's: " + user);
+		users.push({ name: user, id: socket.id });
+		console.log('users logged in:', users);
+		socket.broadcast.emit('message', {
+			author: 'Chat Bot',
+			content: user + ' has joined the conversation!',
+		});
+	});
+
+	socket.on('message', (message) => {
+		console.log("Oh, I've got something from " + socket.id);
+		messages.push(message);
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Oh, socket ' + socket.id + ' has left');
+		const index = users.findIndex((element) => element.id === socket.id);
+		const name = users[index].name;
+		users.splice(index, 1);
+		console.log('users logged in:', users);
+		socket.broadcast.emit('message', {
+			author: 'Chat Bot',
+			content: name + ' has left the conversation... :(',
+		});
+	});
 });
