4e5e434f4a931cd0601909675722667733859308
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-03-04 14:26:35 +0100

    add app.js and server.js

--- client/app.js
+++ client/app.js
@@ -4,3 +4,43 @@ const messagesList = document.querySelector('#messages-list');
 const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
+
+
+let userName;
+
+const login = (e) => {
+	e.preventDefault();
+	if (!userNameInput.value) {
+		alert('Must not be empty !');
+	} else {
+		userName = userNameInput.value;
+		loginForm.classList.remove('show');
+		messagesSection.classList.add('show');
+	}
+};
+
+const sendMessage = (e) => {
+	e.preventDefault();
+	if (!messageContentInput.value) {
+		alert('You must write the message');
+	} else {
+		addMessage(userName, messageContentInput.value);
+		messageContentInput.value = '';
+	}
+};
+
+const addMessage = (author, content) => {
+	const message = document.createElement('li');
+	message.classList.add('message', 'message--received');
+	if (author === userName) {
+		message.classList.add('message--self');
+	}
+	message.innerHTML = ` <h3 class="message__author">${author === userName ? 'You' : author
+		}</h3>
+    <div class="message__content">${content}</div>
+  </li>`;
+	messagesList.appendChild(message);
+};
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+const messages = [];
+
+app.get('/', (_req, res) => {
+	res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+app.listen(8000, () => {
+	console.log('server running on port 8000');
+});
